095f21b9c746a5fec3fe619765ed18968b8ee037
David <davesus33@gmail.com>
2023-10-05 21:37:19 -0700

    sizeof

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+/*This program prints out a quote aboutf text about programming*/
 
 int main(void)
 {
 	char str[58];
+
 	strcpy(str, "\"Programming is like building a multilingual puzzle.");
 
 
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(void){
+	int type_int;
+	float type_float;
+	char type_char;
+	long int type_l_int;
+	long long int type_l_l_int;
+
+	printf("Size of a char:%lu byte(s)\n", sizeof(type_char));
+	printf("Size of an int:%lu byte(s)\n", sizeof(type_int));
+	printf("Size of a long int:%lu byte(s)\n", sizeof(type_l_int));
+	printf("Size of a long long int:%lu byte(s)\n", sizeof(type_l_l_int));
+	printf("Size of a float:%lu byte(s)\n", sizeof(type_float));
+	
+	return (0);
+}
