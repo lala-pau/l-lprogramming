b64a6014c31a344b05ca320da016c442cc836314
David <davesus33@gmail.com>
2023-10-05 18:59:43 -0700

    preprocess me

---
+++ 0x00-hello_world/first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*This code is to display "This is my first C project". In other words, send to standard output*/
+
+int main(void)
+{
+	printf("This is my first C project")
+	return(0)
+
+
+}
