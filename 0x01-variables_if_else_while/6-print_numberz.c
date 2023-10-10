54fb2e423d5d8f4f17c9c63711eb1b69c22ad812
David <davesus33@gmail.com>
2023-10-09 17:42:24 -0700

    1-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - prints out single numbers of base 10
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+
+	}
+	putchar('\n');
+	return (0);
+}
