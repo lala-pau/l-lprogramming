8834119c297a6e7d41baadcfd70194b8e1f74f0e
David <davesus33@gmail.com>
2023-10-14 07:12:56 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  *main - prints out "_putchar"
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar(_putchar);
+	_putchar("\n");
 	return (0);
 }
