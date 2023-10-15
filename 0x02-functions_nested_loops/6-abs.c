d39bcc0442c9da216ab2afde93b2fd7f441356e9
David <davesus33@gmail.com>
2023-10-14 17:25:53 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_abs - displays absolute value of input
+ *@int: value entered
+ *Return: result of abs
+ */
+int _abs(int i)
+{
+	if (i == 0)
+	{
+		i = 0;
+	}
+	else if (i > 0)
+	{
+		i = i;
+	}
+	else if (i < 0)
+	{
+		i = i * -1;
+	}
+	return (i);
+}
