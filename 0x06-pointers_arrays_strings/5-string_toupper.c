dc4d1e1a3ddd575d1c14601c13804d4d1b540e2d
David <davesus33@gmail.com>
2023-10-19 17:04:36 -0700

    lower to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts lowercase to uppercase
+ * @str: the string
+ * Return: the uppercase string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
