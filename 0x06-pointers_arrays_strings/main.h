5c600e856f2e9722512d6b3632e74e67ccbc3261
David <davesus33@gmail.com>
2023-10-19 18:11:36 -0700

    print input number

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,9 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *_strncpy(char *dest, char *src, int n);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
