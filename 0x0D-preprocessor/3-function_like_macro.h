ec535ddf5e35d57d5ea139a0a4c933e100ee8bfc
David <davesus33@gmail.com>
2023-11-06 16:24:47 -0800

    function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef ABS_H
+#define ABS_H
+
+/**
+  * File: 3-function_like_macro
+  * @X: parameter
+  * Desc: produces the absolute value of the parameter
+  */
+
+#define ABS(X) ((X) < 0 ? -(1) * (X) : (X))
+
+#endif
