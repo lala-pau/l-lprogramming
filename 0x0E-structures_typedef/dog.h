4b550f0de1bf9280983ab3cb0946a0812bdbd9d9
David <davesus33@gmail.com>
2023-11-06 18:01:49 -0800

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,17 @@
   * @owner: third member
   * Description: the name, age of the dog. The owner's name too
   */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
 int _strlen(char *s);
 
 #endif
