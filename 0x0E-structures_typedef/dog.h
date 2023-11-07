d500a7592de01b157e3d729583202791852d5cc8
David <davesus33@gmail.com>
2023-11-06 17:25:24 -0800

    dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - dog profile
+  * @name: First member
+  * @age: Second member
+  * @owner: third member
+  * Description: the name, age of the dog. The owner's name too
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+
+#endif
